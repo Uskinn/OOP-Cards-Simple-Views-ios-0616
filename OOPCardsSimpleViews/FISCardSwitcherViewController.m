c796e62575c66db0ff2f30eaf647f05c56e8d86a
Uskinn <s.nevzor@yahoo.com>
2016-06-16 15:35:56 -0400

    Done

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -3,6 +3,15 @@
 #import "FISCardSwitcherViewController.h"
 
 @interface FISCardSwitcherViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+
+- (IBAction)threeOfSpadesButton:(id)sender;
+- (IBAction)fourOfClubsButton:(id)sender;
+- (IBAction)eightOfDiamondsButton:(id)sender;
+- (IBAction)tenOfHeartsButton:(id)sender;
+
 
 @end
 
@@ -12,6 +21,10 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    
+    
+    
     self.threeOfSpades = [[FISCard alloc] initWithSuit:@"♠️"
                                                   rank:@"3"];
     self.fourOfClubs = [[FISCard alloc] initWithSuit:@"♣️"
@@ -22,4 +35,40 @@
                                                 rank:@"10"];
 }
 
+
+
+- (IBAction)threeOfSpadesButton:(id)sender {
+    
+   // NSLog(@"Button tapped");
+    
+    if (self.threeOfSpades) {
+        
+        ((self.topLabel.text = @"♠️3") && (self.middleLabel.text = @"♠️3") && (self.bottomLabel.text = @"♠️3"));
+            
+    }
+}
+
+- (IBAction)fourOfClubsButton:(id)sender {
+    
+    if (self.threeOfSpades) {
+        
+        ((self.topLabel.text = @"♣️4") && (self.middleLabel.text = @"♣️4") && (self.bottomLabel.text = @"♣️4"));
+        
+    }
+}
+
+- (IBAction)eightOfDiamondsButton:(id)sender {
+    
+    if (self.eightOfDiamonds) {
+        ((self.topLabel.text = @"♦️8") && (self.middleLabel.text = @"♦️8") && (self.bottomLabel.text = @"♦️8"));
+    }
+    
+}
+
+- (IBAction)tenOfHeartsButton:(id)sender {
+    
+    if (self.tenOfHearts) {
+        ((self.topLabel.text = @"♥️10") && (self.middleLabel.text = @"♥️10") && (self.bottomLabel.text = @"♥️10"));
+    }
+}
 @end
