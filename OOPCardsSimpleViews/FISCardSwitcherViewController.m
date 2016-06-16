1e221c85b23b93675909fd0371d85afbfbad2958
Uskinn <s.nevzor@yahoo.com>
2016-06-16 16:21:35 -0400

    Polished

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -39,36 +39,54 @@
 
 - (IBAction)threeOfSpadesButton:(id)sender {
     
-   // NSLog(@"Button tapped");
+//    if (self.threeOfSpades) {
+//        self.bottomLabel.backgroundColor = [UIColor orangeColor];
+//        ((self.topLabel.text = @"♠️3") && (self.middleLabel.text = @"♠️3") && (self.bottomLabel.text = @"♠️3"));
+//
     
-    if (self.threeOfSpades) {
-        
-        ((self.topLabel.text = @"♠️3") && (self.middleLabel.text = @"♠️3") && (self.bottomLabel.text = @"♠️3"));
-            
+    self.topLabel.text = self.threeOfSpades.cardLabel;
+    self.middleLabel.text = self.threeOfSpades.cardLabel;
+    self.bottomLabel.text = self.threeOfSpades.cardLabel;
+    
+    self.view.backgroundColor = [UIColor yellowColor];
     }
-}
+
 
 - (IBAction)fourOfClubsButton:(id)sender {
     
-    if (self.threeOfSpades) {
-        
-        ((self.topLabel.text = @"♣️4") && (self.middleLabel.text = @"♣️4") && (self.bottomLabel.text = @"♣️4"));
-        
-    }
+//    if (self.threeOfSpades) {
+//        ((self.topLabel.text = @"♣️4") && (self.middleLabel.text = @"♣️4") && (self.bottomLabel.text = @"♣️4"));
+//        }
+    self.topLabel.text = self.fourOfClubs.cardLabel;
+    self.middleLabel.text = self.fourOfClubs.cardLabel;
+    self.bottomLabel.text = self.fourOfClubs.cardLabel;
+    
+    self.view.backgroundColor = [UIColor blueColor];
+
 }
 
 - (IBAction)eightOfDiamondsButton:(id)sender {
     
-    if (self.eightOfDiamonds) {
-        ((self.topLabel.text = @"♦️8") && (self.middleLabel.text = @"♦️8") && (self.bottomLabel.text = @"♦️8"));
-    }
+//    if (self.eightOfDiamonds) {
+//        ((self.topLabel.text = @"♦️8") && (self.middleLabel.text = @"♦️8") && (self.bottomLabel.text = @"♦️8"));
+//    }
+
+    self.topLabel.text = self.eightOfDiamonds.cardLabel;
+    self.middleLabel.text = self.eightOfDiamonds.cardLabel;
+    self.bottomLabel.text = self.eightOfDiamonds.cardLabel;
     
+    self.view.backgroundColor = [UIColor greenColor];
 }
 
 - (IBAction)tenOfHeartsButton:(id)sender {
-    
-    if (self.tenOfHearts) {
-        ((self.topLabel.text = @"♥️10") && (self.middleLabel.text = @"♥️10") && (self.bottomLabel.text = @"♥️10"));
-    }
+//    
+//    if (self.tenOfHearts) {
+//        ((self.topLabel.text = @"♥️10") && (self.middleLabel.text = @"♥️10") && (self.bottomLabel.text = @"♥️10"));
+//    }
+    self.topLabel.text = self.tenOfHearts.cardLabel;
+    self.middleLabel.text = self.tenOfHearts.cardLabel;
+    self.bottomLabel.text = self.tenOfHearts.cardLabel;
+
+    self.view.backgroundColor = [UIColor lightGrayColor];
 }
 @end
